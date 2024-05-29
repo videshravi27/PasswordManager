0b15fa8a94749aa08e762053989e837919e2c168
Videsh Ravi <videshravi27@gmail.com>
2024-05-29 23:41:05 +0530

    JWT Token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -12,6 +14,53 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
-})
+}, { timestamps: true })
+
+//Static signup
+userSchema.statics.signup = async function(email, password) {
+
+    //validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email is not valid')
+    }
+    // if(!validator.isStrongPassword(password)){
+    //     throw Error('Password is not strong enough')
+    // }
+
+    const exists = await this.findOne({ email })
+
+    if(exists){
+        throw Error('Email already Signedup, Login')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+}
+
+//static login
+userSchema.statics.login = async function(email, password){
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect Email')
+    }
+    const match = await bcrypt.compare(password, user.password)
+    
+    if(!match){
+        throw Error('Incorrect Password')
+    }
+    
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
