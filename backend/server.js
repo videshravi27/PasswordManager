04c7b48f55a5a36451ddb195e4a7f03829848079
Videsh Ravi <videshravi27@gmail.com>
2024-06-10 22:34:34 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const cors = require('cors')
+
 const express = require('express')
 const mongoose = require('mongoose')
 const detailRoutes = require('./routes/details')
@@ -8,6 +10,16 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
+
+app.use(cors(
+    {
+        origin: ["https://password-manager-alpha-bay.vercel.app"],
+        methods: ["POST", "GET","PATCH","DELETE"],
+        credentials: true
+    }
+));
 
 app.use((req, res, next) =>{
     console.log(req.path, req.method)
